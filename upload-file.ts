a3339a7c09c57a5d087fc3c1549e32f7e9e20377
Ana Gauna <amgauna@gmail.com>
2021-11-03 15:33:54 -0300

    Create upload-file.ts

---
+++ upload-file.ts
@@ -0,0 +1,5 @@
+const formData: FormData = new FormData();
+formData.append('Image', image, image.name);
+formData.append('ComponentId', componentId);
+return this.http.post('/api/dashboard/UploadImage', formData);
+
