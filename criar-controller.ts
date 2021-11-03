7d58b819125567b4d9e20d0ac18b4c0e6ba22dbb
Ana Gauna <amgauna@gmail.com>
2021-11-03 15:40:25 -0300

    Create criar-controller.ts

---
+++ criar-controller.ts
@@ -0,0 +1,14 @@
+<div ng-app="myApp" ng-controller="myCtrl">
+{{ firstName + " " + lastName }}
+</div>
+
+<script>
+
+var app = angular.module("myApp", []);
+
+app.controller("myCtrl", function($scope) {
+  $scope.firstName = "John";
+  $scope.lastName = "Doe";
+});
+
+</script>
